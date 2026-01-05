23787821f0b931c09a1f3f6726f115bbb538f4da
mo-hossam-stack <hamo08521hamo@gmail.com>
2026-01-06 00:22:58 +0200

    integrate jinja2 templates  with fastapi

--- app/main.py
+++ app/main.py
@@ -1,7 +1,24 @@
-from fastapi import FastAPI
+import pathlib
+from fastapi import FastAPI , Request
+from fastapi.responses import HTMLResponse 
+from fastapi.templating import Jinja2Templates
+
 
 app = FastAPI()
+BASE_DIR = pathlib.Path(__file__).parent
+#print((BASE_DIR / "templates").exists())
+templates = Jinja2Templates(directory=str(BASE_DIR / "templates"))
 
-@app.get("/")
-def home_view():
+
+@app.get("/" , response_class=HTMLResponse)
+def home_view(request : Request): # http GET -> JSON
+    #print(request)
+    return templates.TemplateResponse("home.html", {"request" : request })
+
+    #return render("home.html" , {}) something like that dose not work  btw i try it for the first time with fastapi
+
+
+
+@app.post("/")
+def home_detail_view():
     return {"hi": "Mohamed"}
