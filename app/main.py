a42c7572802304bffa278343758a830e06ca90a0
mo-hossam-stack <hamo08521hamo@gmail.com>
2026-01-08 16:33:05 +0200

    feat: add settings management and update requirements for pydantic-settings

--- app/main.py
+++ app/main.py
@@ -1,7 +1,22 @@
 import pathlib
-from fastapi import FastAPI , Request
+from fastapi import FastAPI , Request , Depends
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from pydantic_settings import BaseSettings
+from functools import lru_cache
+
+class Settings(BaseSettings):
+    DEBUG: bool = False
+
+    class Config:
+        env_file = ".env"
+
+@lru_cache()
+def get_settings():
+    return Settings() # make sure to cache the settings instance
+
+settings = get_settings()
+DEBUG = settings.DEBUG
 
 
 app = FastAPI()
@@ -11,7 +26,7 @@ templates = Jinja2Templates(directory=str(BASE_DIR / "templates"))
 
 
 @app.get("/" , response_class=HTMLResponse)
-def home_view(request : Request): # http GET -> JSON
+def home_view(request : Request , settings:Settings = Depends(get_settings)): # http GET -> JSON
     #print(request)
     return templates.TemplateResponse(request, "home.html")
 
