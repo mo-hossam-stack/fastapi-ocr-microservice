d3cf35e84ad7277785660876a654533a5488cd96
mo-hossam-stack <hamo08521hamo@gmail.com>
2026-01-11 15:10:09 +0200

    feat: add image upload functionality and update requirements for python-multipart

--- app/main.py
+++ app/main.py
@@ -1,6 +1,12 @@
 import pathlib
-from fastapi import FastAPI , Request , Depends
-from fastapi.responses import HTMLResponse
+import io
+import uuid
+from fastapi import (FastAPI,
+                    Request ,
+                    Depends,
+                    File,
+                    UploadFile)
+from fastapi.responses import HTMLResponse , FileResponse
 from fastapi.templating import Jinja2Templates
 from pydantic_settings import BaseSettings
 from functools import lru_cache
@@ -21,6 +27,7 @@ DEBUG = settings.DEBUG
 
 app = FastAPI()
 BASE_DIR = pathlib.Path(__file__).parent
+UPLOAD_DIR = BASE_DIR / "uploads"
 #print((BASE_DIR / "templates").exists())
 templates = Jinja2Templates(directory=str(BASE_DIR / "templates"))
 
@@ -37,3 +44,14 @@ def home_view(request : Request , settings:Settings = Depends(get_settings)): # 
 @app.post("/")
 def home_detail_view():
     return {"hi": "Mohamed"}
+
+@app.post("/image-upload/" , response_class=FileResponse)
+async def image_upload_view(file: UploadFile = File(...)):
+    bytes_str = io.BytesIO(await file.read()) # read file as bytes
+    fname = pathlib.Path(file.filename)
+    fext = fname.suffix # get file extension like .jpg , .png etc.....:)
+    dest = UPLOAD_DIR / f"{uuid.uuid1()}{fext}" # create a unique file name
+    with open(dest, "wb") as f:
+        f.write(bytes_str.read())
+
+    return dest
